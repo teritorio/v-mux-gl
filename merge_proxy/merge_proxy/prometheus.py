18729155a1c51b6931d37c66f50b6006b8790f47
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2022-04-21 11:41:31 +0200

    fix naming

--- merge_proxy/merge_proxy/prometheus.py
+++ merge_proxy/merge_proxy/prometheus.py
@@ -24,7 +24,7 @@ def merge_instruments():
     prometheus_summary_fetch_full = []
     prometheus_summary_fetch_partial = []
     prometheus_summary_merge = []
-    prometheus_summary_build_tile = []
+    prometheus_summary_build = []
     for z in range(0, 14 + 1):
         prometheus_summary_fetch_full.append(
             prometheus_client.Summary(f"fetch_full_z{z}", f"Zoom {z} fetch full tile")
@@ -37,13 +37,13 @@ def merge_instruments():
         prometheus_summary_merge.append(
             prometheus_client.Summary(f"merge_z{z}", f"Zoom {z} merge")
         )
-        prometheus_summary_build_tile.append(
-            prometheus_client.Summary(f"build_tile_z{z}", f"Zoom {z} build tile")
+        prometheus_summary_build.append(
+            prometheus_client.Summary(f"build_z{z}", f"Zoom {z} build")
         )
 
     return [
         prometheus_summary_fetch_full,
         prometheus_summary_fetch_partial,
         prometheus_summary_merge,
-        prometheus_summary_build_tile,
+        prometheus_summary_build,
     ]
